5115eab52617b6a0b30addfaa3cc545f5aa06257
Snövit <ville.nystrm@mail.com>
2015-02-12 11:24:51 +0100

    start

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+       
+        ?>
+        
+        <script src="jquery-1.11.2.min.js"></script>
+        <script src="JS1.js"></script>
+    </body>
+    
+</html>
